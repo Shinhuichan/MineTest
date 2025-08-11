bea01c0bbd7fe8ddbde12321f0e500b71771b096
Shinhuichan <94081602shc@gmail.com>
2025-08-11 12:18:19 +0900

    Revert "v0.0.2 edit: object socket interaction (by.soohyun)"
    This reverts commit cddcc4f614a60df863b0df9a2750211aaf02f761, reversing
    changes made to 234cba8e574526a1a18b1e60ad9b0009e116b671.

--- Assets/3.Script/LSH/ObjectData/ObjectData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName ="Object", menuName ="ObjectData")]
-public class ObjectData : ScriptableObject
-{
-    public string objectName;
-    public string objectDescription;
-    public Sprite objectSprite;
-}
