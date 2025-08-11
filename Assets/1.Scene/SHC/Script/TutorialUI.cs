41dd8a34a80ba32be76b64c4a7c1ac2313aa13db
Shinhuichan <94081602shc@gmail.com>
2025-08-11 15:43:55 +0900

    v0.0.5 feat: Setting UI 기본 틀 작업

---
+++ Assets/1.Scene/SHC/Script/TutorialUI.cs
@@ -0,0 +1,80 @@
+using CustomInspector;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public enum IntroduceType
+{
+    Image,
+    Video
+}
+
+[System.Serializable]
+public struct Introduce
+{
+    public IntroduceType type;
+    public Sprite sprite;
+    public VideoClip videoClip;
+    [TextArea] public string introduceText;
+}
+
+public class TutorialUI : MonoBehaviour
+{
+    public Introduce[] introduces;
+
+    [Header("Show Component Setup")]
+    public Image imageUI;
+    public VideoPlayer videoPlayer;
+    public TextMeshProUGUI textUI;
+
+    [Header("UI Show Index")]
+    [SerializeField, ReadOnly] private int currentIndex = 0;
+
+    private void Start()
+    {
+        ShowIntroduce(currentIndex);
+    }
+
+    public void ShowIntroduce(int index)
+    {
+        if (index < 0 || index >= introduces.Length) return;
+
+        Introduce current = introduces[index];
+        textUI.text = current.introduceText;
+
+        // Type이 Image인 경우
+        if (current.type == IntroduceType.Image)
+        {
+            imageUI.gameObject.SetActive(true);
+            videoPlayer.gameObject.SetActive(false);
+
+            imageUI.sprite = current.sprite;
+        }
+        // Type이 Video인 경우
+        else if (current.type == IntroduceType.Video)
+        {
+            imageUI.gameObject.SetActive(false);
+            videoPlayer.gameObject.SetActive(true);
+
+            videoPlayer.clip = current.videoClip;
+            videoPlayer.Play();
+        }
+    }
+
+    #region UI 전환 메서드
+    public void Next()
+    {
+        currentIndex++;
+        if (currentIndex >= introduces.Length) currentIndex = 0;
+        ShowIntroduce(currentIndex);
+    }
+
+    public void Prev()
+    {
+        currentIndex--;
+        if (currentIndex < 0) currentIndex = introduces.Length - 1;
+        ShowIntroduce(currentIndex);
+    }
+    #endregion
+}
