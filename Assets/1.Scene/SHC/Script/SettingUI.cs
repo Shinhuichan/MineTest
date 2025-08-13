2c4cdf08b84a0de798ea778d051402955ce1fb53
Shinhuichan <94081602shc@gmail.com>
2025-08-13 10:22:02 +0900

    v0.0.11 각자한것들 합치기

--- Assets/1.Scene/SHC/Script/SettingUI.cs
+++ Assets/1.Scene/SHC/Script/SettingUI.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 
 public class SettingUI : MonoBehaviour
 {
+    [SerializeField] GameObject menuUI;
     [SerializeField] GameObject settingUI;
     [SerializeField, ReadOnly] private bool toggleBool;
     public void SetToggleUI()
     {
-        settingUI.SetActive(!toggleBool);
+        menuUI.SetActive(!toggleBool);
         toggleBool = !toggleBool;
+    }
+
+    public void EnterSettingUI()
+    {
+        settingUI.SetActive(true);
+    }
+    public void ExitSettingUI()
+    {
+        settingUI.SetActive(false);
     }
 }
