50511b82469dd73eaae4a4d78b3b113be54e8451
heosoomi <wellmaderice23@gmail.com>
2025-08-26 13:00:08 +0900

    v.0.0.16

---
+++ Assets/1.Scene/HSM/Script/UI/BoardSwicher.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class BoardSwicher : MonoBehaviour
+{
+    public Transform BoardA;
+    public Transform BoardB;
+
+    public float animDuration = 0.7f;
+    //목표 위치를 저장할 변수
+    public Vector3 PosA_Front;
+    public Vector3 PosA_Back;
+    public Vector3 PosB_Front;
+    public Vector3 PosB_Back;
+
+    [Header("Buttons")]
+    public Button mineral_View_Button;
+    public Button mineral_Table_Button;
+
+    [Header("UIs")]
+    public GameObject mineral_View;
+    public GameObject mineral_Table;
+
+    private bool isBInFront = false;
+    private bool isViewTrue = true;
+    private void Start()
+    {
+        // 시작할때의 위치를 기반으로 앞 ,뒤 위치를 정의
+        PosA_Front = BoardA.position;
+        PosB_Back = BoardB.position;
+
+        // 서로의 위치를 목표 지접으로 설정
+        PosA_Back = PosB_Back;
+        PosB_Front = PosA_Front;
+
+        mineral_View_Button.onClick.AddListener(ViewChange);
+        mineral_Table_Button.onClick.AddListener(ViewChange);
+
+        mineral_View.SetActive(isViewTrue);
+        mineral_Table.SetActive(!isViewTrue);
+    }
+
+    public void ViewChange()
+    {
+        mineral_View.SetActive(isViewTrue);
+        mineral_Table.SetActive(!isViewTrue);
+
+        OnSwapButtonClick();
+    }
+
+    public void OnSwapButtonClick()
+    {
+        isBInFront = !isBInFront;
+        if(isBInFront)
+        {
+            // B를 앞으로, A를 뒤로 보냄
+            //.SetEase()를 통해 부드러운 움직임 종류를 선택 할 수 있습니다.
+
+            BoardA.DOMove(PosA_Back, animDuration).SetEase(Ease.OutQuad);
+            BoardB.DOMove(PosB_Front, animDuration).SetEase(Ease.OutQuad);
+        }
+        else
+        {
+            // A를 앞으로, B를 뒤로 보냄
+            BoardA.DOMove(PosA_Front, animDuration).SetEase(Ease.OutQuad);
+            BoardB.DOMove(PosB_Back, animDuration).SetEase(Ease.OutQuad);
+        }
+    }
+}
